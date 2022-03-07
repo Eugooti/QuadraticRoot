4cc281e73bb29cbc656430ff1bcbac600e9c2ce5
Eugooti <eugeneotieno003@gmail.com>
2022-03-07 12:22:08 +0300

    Quadratic roots

---
+++ src/com/eugene/Main.java
@@ -0,0 +1,39 @@
+package com.eugene;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        double a, b, c;
+        System.out.println("Enter coefficients:");
+        System.out.print("A: ");
+        a=scanner.nextDouble();
+        System.out.print("B: ");
+        b=scanner.nextDouble();
+        System.out.print("C: ");
+        c=scanner.nextDouble();
+
+        System.out.println(roots(a, b, c));
+
+    }
+
+    public static Point roots(double a, double b,double c){
+        Point point=new Point();
+        double roots=(Math.pow(b, 2))-(4*a*c);
+
+        if (roots==0){
+            point.x= (int) (-b/(2*a));
+            point.y= (int) (-b/(2*a));
+        }
+
+        else if (roots>0){
+            point.x= (int) ((-b+Math.sqrt(roots))/(2*a));
+            point.y= (int) ((-b-Math.sqrt(roots))/(2*a));
+        }
+
+        return point;
+    }
+}
